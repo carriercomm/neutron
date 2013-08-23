5915e2791ebd96128267f7da84ff4ff55077c38d
Dan Florea <dflorea@cisco.com>
2013-08-22 17:40:08 -0700

    Add DSCP option to Security Groups
    This commit adds the DSCP option for Security Groups to the API
    and the database.
    
    Change-Id: Ica26cf439e42ca95bf1f0ff731337f6ad4b91f5e
    Implements: blueprint security-groups-dscp-filter
    
    Conflicts:
    	neutron/tests/unit/test_extension_security_group.py
    	neutron/tests/unit/test_security_groups_rpc.py

--- neutron/agent/linux/iptables_firewall.py
+++ neutron/agent/linux/iptables_firewall.py
@@ -297,6 +297,7 @@ class IptablesFirewallDriver(firewall.FirewallDriver):
                                    rule.get('protocol'),
                                    rule.get('port_range_min'),
                                    rule.get('port_range_max'))
+            args += self._dscp_arg(rule.get('dscp'))
             args += ['-j RETURN']
             iptables_rules += [' '.join(args)]
 
@@ -313,6 +314,12 @@ class IptablesFirewallDriver(firewall.FirewallDriver):
         # Allow established connections
         iptables_rules += ['-m state --state RELATED,ESTABLISHED -j RETURN']
         return iptables_rules
+
+    def _dscp_arg(self, dscp):
+        if not dscp:
+            return []
+        iptables_rule = ['-m', 'dscp', '--dscp', '%s' % dscp]
+        return iptables_rule
 
     def _protocol_arg(self, protocol):
         if not protocol:
