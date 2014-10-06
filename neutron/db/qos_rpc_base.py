dc3d37560697c161596e04f16278c5d6cb6c456a
Sean M. Collins <sean_collins2@cable.comcast.com>
2014-10-06 15:01:48 -0400

    QoS API Extension Agent & Plugin RPC and QoS drivers
    Change-Id: If72a31b685d9ccd4d4b6378b47c5db26d2879b58

---
+++ neutron/db/qos_rpc_base.py
@@ -0,0 +1,127 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright 2013 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# @author: Sean M. Collins, sean@coreitpro.com, Comcast #
+import sqlalchemy as sa
+from sqlalchemy import orm
+from sqlalchemy.orm import exc
+
+from neutron.db import qos_db
+from neutron.extensions import qos as ext_qos
+
+
+
+class QoSServerRpcMixin(qos_db.QoSDbMixin):
+
+    def _process_create_qos_for_network(self, context, qos_id, network_id):
+        super(QoSServerRpcMixin, self).create_qos_for_network(
+            context, qos_id, network_id)
+        self.notifier.network_qos_updated(context, qos_id, network_id)
+
+    def _process_create_qos_for_port(self, context, qos_id, port_id):
+        self.notifier.port_qos_updated(context, qos_id, port_id)
+
+    def _process_delete_qos_for_network(self, context, qos_id, network_id):
+        super(QoSServerRpcMixin, self).delete_qos_for_network(
+            context, network_id)
+        self.notifier.network_qos_deleted(context, qos_id, network_id)
+
+    def _process_delete_qos_for_port(self, context, qos_id, port_id):
+        self.notifier.port_qos_deleted(context, qos_id, port_id)
+
+    def _process_update_mapping_for_network(self, context, mapping):
+        super(QoSServerRpcMixin, self).update_mapping_for_network(
+            context, mapping)
+        self.notifier.network_qos_updated(context,
+                                          mapping.qos_id,
+                                          mapping.network_id)
+
+    def _process_update_mapping_for_port(self, context, mapping):
+        super(QoSServerRpcMixin, self).update_mapping_for_port(
+            context, mapping)
+        self.notifier.port_qos_updated(context,
+                                       mapping.qos_id,
+                                       mapping.port_id)
+
+    def update_qos(self, context, id, qos):
+        result = super(QoSServerRpcMixin, self).update_qos(context,
+                                                           id,
+                                                           qos)
+        qos_item = self._get_by_id(context, qos_db.QoS, id)
+        for port_mapping in qos_item.ports:
+            self.notifier.port_qos_updated(context,
+                                           id,
+                                           port_mapping['port_id'])
+        for net_mapping in qos_item.networks:
+            self.notifier.network_qos_updated(context,
+                                              id,
+                                              net_mapping['network_id'])
+        return result
+
+    def delete_qos(self, context, id):
+        qos_item = self._get_by_id(context, qos_db.QoS, id)
+        for port_mapping in qos_item.ports:
+            self.notifier.port_qos_deleted(context,
+                                           id,
+                                           port_mapping['port_id'])
+
+        for net_mapping in qos_item.networks:
+            self.notifier.network_qos_deleted(context,
+                                              id,
+                                              net_mapping['network_id'])
+        super(QoSServerRpcMixin, self).delete_qos(context, id)
+
+    def process_port_qos_update(self, context, id, port):
+        if ext_qos.QOS not in port['port']:
+            return
+        mapping = self.get_mapping_for_port(context, id)
+        qos_id = super(QoSServerRpcMixin, self).process_port_qos_update(context, id, port)
+        if qos_id is None:
+            self._process_delete_qos_for_port(context, mapping[0].qos_id, id)
+        elif mapping == [] and qos_id is not None:
+            self._process_create_qos_for_port(context, qos_id, id)
+        elif mapping != [] and qos_id != mapping[0].qos_id:
+            self._process_update_mapping_for_port(context, mapping[0])
+
+
+class QoSServerRpcCallbackMixin(object):
+
+    def get_policy_for_qos(self, context, **kwargs):
+        result = {}
+        qos_id = kwargs.get('qos_id')
+        query = context.session.query(qos_db.QoS)
+        results = query.filter_by(id=qos_id)
+        for policy in results.one().policies:
+            result[policy['key']] = policy['value']
+        return result
+
+    def get_qos_by_network(self, context, **kwargs):
+        network_id = kwargs.get('network_id')
+        query = context.session.query(qos_db.NetworkQoSMapping)
+        try:
+            mapping = query.filter_by(network_id=network_id).one()
+            return mapping.qos_id
+        except exc.NoResultFound:
+            return []
+
+    def get_qos_by_port(self, context, **kwargs):
+        port_id = kwargs.get('port_id')
+        query = context.session.query(qos_db.PortQoSMapping)
+        try:
+            mapping = query.filter_by(port_id=port_id).one()
+            return mapping.qos_id
+        except exc.NoResultFound:
+            return []
